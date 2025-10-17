36e5c6df1dd0238d90ce0a9969ae30d00b540bcf
Lucaferr <luca.ferreira@estudiantes.utec.edu.uy>
2025-10-17 16:06:18 -0300

    f

--- lab/users/models.py
+++ lab/users/models.py
@@ -3,14 +3,12 @@
 from django.db import models
 
 class Usuario(models.Model):
-    # Django crea automáticamente el campo id (PK)
 
     nombre1 = models.CharField(max_length=50)        # NOT NULL por defecto si no se usa null=True
     nombre2 = models.CharField(max_length=50, blank=True, null=True) # Se permite vacío
     apellido1 = models.CharField(max_length=50)
     apellido2 = models.CharField(max_length=50)
     
-    # *** AQUÍ ESTÁ LA CLAVE: unique=True ***
     email = models.EmailField(max_length=200, unique=True)
     
     telefono = models.CharField(max_length=9) 
